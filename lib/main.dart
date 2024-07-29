3524250c61f221574cb8d8a2408388d3105a56ce
SDUYRG <202200300050@mail.sdu.edu.cn>
2024-07-29 15:15:42 +0800

    实现空安全

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'constants.dart';
+import 'controllers/MenuController.dart' as prefix0;
+import 'screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => prefix0.MenuController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
